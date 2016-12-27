ae0b9a19412f6971f82ec5e594937aa84b744be6
Slaik <slaik000@live.com>
2016-12-27 03:48:14 +0300

    update to gulp

--- src/MSStories/gulpfile.js
+++ src/MSStories/gulpfile.js
@@ -1,56 +1,69 @@
-﻿"use strict";
+﻿'use strict';
 
-var gulp = require("gulp"),
-    rimraf = require("rimraf"),
-    concat = require("gulp-concat"),
-    cssmin = require("gulp-cssmin"),
-    uglify = require("gulp-uglify"),
-    sass = require("gulp-sass"),
-    autoprefixer = require("autoprefixer"),
-    cssnano = require("cssnano"),
-    postcss = require("gulp-postcss"),
-    stylelint = require("stylelint"),
-    reporter = require("postcss-reporter"),
+var gulp = require('gulp'),
+    rimraf = require('rimraf'),
+    concat = require('gulp-concat'),
+    cssmin = require('gulp-cssmin'),
+    uglify = require('gulp-uglify'),
+    sass = require('gulp-sass'),
+    autoprefixer = require('autoprefixer'),
+    cssnano = require('cssnano'),
+    postcss = require('gulp-postcss'),
+    stylelint = require('stylelint'),
+    reporter = require('postcss-reporter'),
     syntax_scss = require('postcss-scss'),
-    focus = require("postcss-focus"),
-    rename = require('gulp-rename');
+    focus = require('postcss-focus'),
+    rename = require('gulp-rename'),
+    gnf = require("gulp-npm-files"),
+    pump = require("pump"),
+    jshint = require("gulp-jshint"),
+    stylish = require("jshint-stylish");
 
 var paths = {
-    webroot: "./wwwroot/"
+    webroot: './wwwroot/',
+    lib: './wwwroot/lib'
 };
 
-paths.js = paths.webroot + "js/**/*.js";
-paths.minJs = paths.webroot + "js/**/*.min.js";
-paths.css = paths.webroot + "css/**/*.css";
-paths.minCss = paths.webroot + "css/**/*.min.css";
-paths.concatJsDest = paths.webroot + "js/site.min.js";
-paths.concatCssDest = paths.webroot + "css/site.css";
-paths.cssMin = paths.webroot + "css/site.min.css";
-paths.sass = './Css/**/*.scss';
-paths.normalize = paths.webroot + "lib/normalize-css/normalize.css";
-paths.cssDir = paths.webroot + 'css';
-paths.jsDir = paths.webroot + 'js';
+paths.js = {
+    js: paths.webroot + 'js/**/*.js',
+    minJs: paths.webroot + 'js/**/*.min.js',
+    jsDir: paths.webroot + 'js',
+    concatName: 'site.js',
+    concat: paths.webroot + 'js/site.js',
+    jsFromTs: './Scripts/js',
+    jsFromTsFiles: './Scripts/js/**/*.js'
+};
+
+paths.css = {
+    css: paths.webroot + 'css/**/*.css',
+    minCss: paths.webroot + 'css/**/*.min.css',
+    concat: paths.webroot + 'css/site.css',
+    cssMin: paths.webroot + 'css/site.min.css',
+    sass: './Css/**/*.scss',
+    normalize: paths.webroot + 'lib/normalize.css/normalize.css',
+    cssDir: paths.webroot + 'css'
+};
 
 var stylelintConfig = {
-    "rules": {
-        "color-hex-case": "lower",
-        "font-family-name-quotes": "always-where-recommended",
-        "number-leading-zero": "never",
-        "number-no-trailing-zeros": true,
-        "string-quotes": "single",
-        "unit-no-unknown": true,
-        "value-no-vendor-prefix": true,
-        "property-no-vendor-prefix": true,
-        "declaration-no-important": true,
-        "declaration-block-no-duplicate-properties": true,
-        "declaration-block-semicolon-newline-after": "always-multi-line",
-        "declaration-block-semicolon-newline-before": "never-multi-line",
-        "declaration-block-semicolon-space-after": "always-single-line",
-        "declaration-block-semicolon-space-before": "never",
-        "declaration-block-trailing-semicolon": "always",
-        "block-closing-brace-newline-after": "always",
-        "block-no-empty": true,
-        "block-opening-brace-newline-after": "always"
+    'rules': {
+        'color-hex-case': 'lower',
+        'font-family-name-quotes': 'always-where-recommended',
+        'number-leading-zero': 'never',
+        'number-no-trailing-zeros': true,
+        'string-quotes': 'single',
+        'unit-no-unknown': true,
+        'value-no-vendor-prefix': true,
+        'property-no-vendor-prefix': true,
+        'declaration-no-important': true,
+        'declaration-block-no-duplicate-properties': true,
+        'declaration-block-semicolon-newline-after': 'always-multi-line',
+        'declaration-block-semicolon-newline-before': 'never-multi-line',
+        'declaration-block-semicolon-space-after': 'always-single-line',
+        'declaration-block-semicolon-space-before': 'never',
+        'declaration-block-trailing-semicolon': 'always',
+        'block-closing-brace-newline-after': 'always',
+        'block-no-empty': true,
+        'block-opening-brace-newline-after': 'always'
 
     }
 };
@@ -60,62 +73,136 @@ var sassOptions = {
     outputStyle: 'expanded'
 };
 
-gulp.task("clean_js", function (cb) {
-    rimraf(paths.concatJsDest, cb);
-});
 
-gulp.task("min_js", function () {
-    return gulp.src([paths.js, "!" + paths.minJs], { base: "." })
-        .pipe(concat(paths.concatJsDest))
-        .pipe(uglify())
-        .pipe(gulp.dest("."));
-});
 
-gulp.task("css_min", gulp.series('css_create_min', function () {
+function css_min() {
     var processors = [cssnano()];
-    return gulp.src(paths.minCss)
+    return gulp.src(paths.css.minCss)
         .pipe(postcss(processors))
-        .pipe(gulp.dest(paths.cssDir));
-}));
+        .pipe(gulp.dest(paths.css.cssDir));
+}
 
-gulp.task('css_create_min', gulp.series('css_concat', function () {
-    return gulp.src(paths.concatCssDest)
-        .pipe(rename(paths.cssMin))
-        .pipe(gulp.dest("."));
-}));
+function css_create_min() {
+    return gulp.src(paths.css.concat, { base: paths.css.cssDir })
+        .pipe(rename(paths.css.cssMin))
+        .pipe(gulp.dest('.'));
+}
 
-gulp.task("css_lint", function () {
+function css_lint() {
     var processors = [
         stylelint(stylelintConfig),
         reporter({ clearMessages: true })
     ];
-    return gulp.src(paths.sass)
+    return gulp.src(paths.css.sass)
         .pipe(postcss(processors, { syntax: syntax_scss }));
-});
+}
 
-gulp.task('css_concat', gulp.series('sass', function () {
-    return gulp.src([paths.normalize, paths.css, "!" + paths.minCss])
-        .pipe(concat(paths.concatCssDest))
-        .pipe(gulp.dest("."));
-}));
+function css_concat() {
+    return gulp.src([paths.css.normalize, paths.css.css, '!' + paths.css.minCss], { base: paths.css.cssDir })
+        .pipe(concat(paths.css.concat))
+        .pipe(gulp.dest('.'));
+}
 
-
-
-gulp.task('sass', gulp.series('css_lint', function () {
+function css_sass() {
     var process = [autoprefixer, focus];
-    return gulp.src(paths.sass)
+    return gulp.src(paths.css.sass)
         .pipe(sass(sassOptions).on('error', sass.logError))
         .pipe(postcss(process))
-        .pipe(gulp.dest(paths.webroot + 'css'));
-}));
+        .pipe(gulp.dest(paths.css.cssDir));
+}
 
-gulp.task('sass_watch', function () {
-    gulp.watch(paths.sass)
-        .on("change", gulp.run('css_build'));
+function css_sass_watch() {
+    gulp.watch(paths.css.sass)
+        .on('change', gulp.run('css_build'));
+}
+
+function copy_libs() {
+
+    return gulp.src(gnf(), {
+        base: "./node_modules"
+    })
+        .pipe(gulp.dest(paths.lib));
+
+}
+
+
+function js_clean() {
+    rimraf(paths.js.concat, cb);
+}
+
+function js_min(cb) {
+    var options = {
+        preserveComments: 'license'
+    };
+    pump([
+        gulp.src(paths.js.minJs, {
+            base: './'
+        }),
+        uglify(),
+        gulp.dest('.')
+    ], cb);
+}
+
+function js_lint() {
+    return gulp.src(paths.js.jsFromTsFiles)
+        .pipe(jshint())
+        .pipe(jshint.reporter(stylish));
+}
+
+function js_concat() {
+    return gulp.src(paths.js.jsFromTsFiles)
+        .pipe(concat(paths.js.concatName))
+        .pipe(gulp.dest(paths.js.jsDir));
+};
+
+function js_create_min() {
+    return gulp.src([paths.js.concat, "!" + paths.js.minJs], {
+        base: './'
+    })
+        .pipe(rename(function (path) {
+            path.basename += ".min";
+        }))
+        .pipe(gulp.dest("."));
+
+
+}
+
+gulp.task("js_min", js_min);
+
+gulp.task("js_lint", js_lint);
+
+gulp.task('js_concat', js_concat);
+
+gulp.task('js_create_min', js_create_min);
+
+gulp.task('copy_libs', copy_libs);
+
+gulp.task('js_clean', js_clean);
+
+gulp.task('js_min', js_min);
+
+gulp.task('css_lint', css_lint);
+
+gulp.task('css_sass', css_sass);
+
+gulp.task('css_concat', css_concat);
+
+gulp.task('css_min', css_min);
+
+gulp.task('css_create_min', css_create_min);
+
+gulp.task('css_sass_watch', css_sass_watch);
+
+let css_build = gulp.series('css_lint', 'css_sass', 'css_concat', 'css_create_min', 'css_min');
+
+let js_build = gulp.series('js_lint', 'js_concat', 'js_create_min', "js_min");
+
+let build = gulp.parallel(css_build, js_build, function (done) {
+    done();
 });
 
-gulp.task("css_build", gulp.series("css_lint", 'sass', 'css_concat', 'css_create_min', 'css_min'));
+gulp.task('css_build', css_build);
 
+gulp.task('js_build', js_build);
 
-
-gulp.task("js", gulp.series('clean_js', 'min_js'));
+gulp.task('build', build);
--- src/MSStories/wwwroot/js/site.js
+++ src/MSStories/wwwroot/js/site.js
@@ -1,1 +1,9 @@
-﻿// Write your Javascript code.
+function io() {
+    var b = true;
+}
+//# sourceMappingURL=main.js.map
+function Back() {
+    var x = 18;
+    x = 29;
+}
+//# sourceMappingURL=sites.js.map
